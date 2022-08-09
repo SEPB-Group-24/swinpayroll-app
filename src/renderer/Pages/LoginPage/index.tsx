cbbddf84ff78065460bb7eb66e8d5af53be2811f
cjuhasz64 <cjuhasz64@gmail.com>
2022-08-09 12:16:57 +1000

    Update index.tsx

--- src/renderer/Pages/LoginPage/index.tsx
+++ src/renderer/Pages/LoginPage/index.tsx
@@ -4,6 +4,7 @@ import logo from './logo.jpg';
 const LoginPage = () => {
   return (
     <body>
+
       <div id="grid-header">
         <img src={logo} />
         <h1>Welcome to SwinPayroll</h1>
@@ -12,23 +13,24 @@ const LoginPage = () => {
       <div id="grid-body">
         <form>
           <h3>Sign In</h3>
-
-          <label>Username</label><br></br>
-          <input type="text" id="username"></input><br></br>
-
-          <label>Password</label><br></br>
-          <input type="text" id="password"></input><br></br>
-
-          <button>SIGN IN</button>
-          <p><a href="">Forgot Password?</a></p>
-
-          <hr></hr>
-          <label>New User?</label><br></br>
+          <label>Username</label>
+          <br/>
+          <input type="text" id="username"></input>
+          <br/>
+          <label>Password</label><br/>
+          <input type="text" id="password"></input>
+          <br/>
+          <button type="submit" value="Submit">SIGN IN</button>
+          <p>
+            <a href="">Forgot Password?</a>
+          </p>
+          <hr/>
+          <label>New User?</label>
+          <br/>
           <button>SIGN UP</button>
-
         </form>
       </div>
-
+      
     </body>
   );
 };
