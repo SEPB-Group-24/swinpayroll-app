cac79fd8d3ccbc4107f0e93f58a0d7dbc3399068
cjuhasz64 <54127982+cjuhasz64@users.noreply.github.com>
2022-08-09 12:33:14 +1000

    Update App.tsx

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,7 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
-import LoginPage from './Pages/LoginPage'
+import LoginPage from './Pages/LoginPage';
 
 const Hello = () => {
   return (
@@ -44,10 +44,6 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route 
-        //path="/" element={<Hello />} 
-        />
-
         <Route path="/" element={<LoginPage />} />
       </Routes>
     </Router>
