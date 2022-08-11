a18be749100846a794aaa00cce024fe862c93321
cjuhasz64 <cjuhasz64@gmail.com>
2022-08-11 17:55:54 +1000

    Update Login Page Path in Route

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
