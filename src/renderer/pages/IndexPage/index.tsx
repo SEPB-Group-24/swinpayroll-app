92287b753101d1f0f1e109c3c8b3d7fa8accf6db
Paves22 <54125532+Paves22@users.noreply.github.com>
2022-09-18 23:44:10 +1000

    Logout functionality

--- src/renderer/pages/IndexPage/index.tsx
+++ src/renderer/pages/IndexPage/index.tsx
@@ -1,34 +1,43 @@
+import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { OnLogout } from 'renderer/components/Auth';
+
 import logo from './logo.jpg';
+
 import './style.scss';
 
 interface Props {
   onLogout: OnLogout;
 }
 
-export default function IndexPage() {
-  return (
-    <div className="IndexPage">
-      <div className="header">
-        <div className="logoContainer">
-          <img src={logo} />
+export default class IndexPage extends Component<Props> {
+  constructor(props:Props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="IndexPage">
+        <div className="header">
+          <div className="logoContainer">
+            <img src={logo} />
+          </div>
+          <div className="headingMain">
+            Welcome to SwinPayroll
+          </div>
         </div>
-        <div className="headingMain">
-          Welcome to SwinPayroll
+        <div className="body">
+          <div className="master">
+            <Link to="/master"><button>Master</button></Link>
+          </div>
+          <div className="masterDetail">
+            <Link to="/"><button>Master-Detail</button></Link>
+          </div>
+          <div className="logout">
+            <Link to="/"><button onClick={() => this.props.onLogout()}>Log Out</button></Link>
+          </div>
         </div>
       </div>
-      <div className="body">
-        <div className="master">
-          <Link to="/master"><button>Master</button></Link>
-        </div>
-        <div className="masterDetail">
-          <Link to="/"><button>Master-Detail</button></Link>
-        </div>
-        <div className="logout">
-          <Link to="/"><button>Log Out</button></Link>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 };
