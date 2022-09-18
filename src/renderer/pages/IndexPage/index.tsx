5afd16c5253e832999116fc6c50511ea55810c61
Paves22 <54125532+Paves22@users.noreply.github.com>
2022-09-18 22:57:49 +1000

    HTML/CSS changes

--- src/renderer/pages/IndexPage/index.tsx
+++ src/renderer/pages/IndexPage/index.tsx
@@ -1,13 +1,33 @@
 import { Link } from 'react-router-dom';
+import { OnLogout } from 'renderer/components/Auth';
+import logo from './logo.jpg';
+import './style.scss';
+
+interface Props {
+  onLogout: OnLogout;
+}
 
 export default function IndexPage() {
   return (
     <div className="IndexPage">
-      <div>
-        <Link to="/login">Login</Link>
+      <div className="header">
+        <div className="logoContainer">
+          <img src={logo} />
+        </div>
+        <div className="headingMain">
+          Welcome to SwinPayroll
+        </div>
       </div>
-      <div>
-        <Link to="/master">Master</Link>
+      <div className="body">
+        <div className="master">
+          <Link to="/master"><button>Master</button></Link>
+        </div>
+        <div className="masterDetail">
+          <Link to="/"><button>Master-Detail</button></Link>
+        </div>
+        <div className="logout">
+          <Link to="/"><button>Log Out</button></Link>
+        </div>
       </div>
     </div>
   );
