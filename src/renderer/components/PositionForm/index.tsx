b66d702aae5858b3b9a9e99c4d53f6663fb716e8
Dean Levinson <dean.e.levinson@gmail.com>
2022-09-25 18:23:34 +1000

    Add validation errors

--- src/renderer/components/PositionForm/index.tsx
+++ src/renderer/components/PositionForm/index.tsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { FetchApi } from 'renderer/components/Auth';
+import InputWrapper from 'renderer/components/InputWrapper';
 import MasterForm from 'renderer/components/MasterForm';
 import { Data } from 'renderer/pages/MasterPage';
 
@@ -11,11 +12,11 @@ export interface Position {
   maximum_pay: string;
 }
 
-interface Props { 
+interface Props {
   fetchApi: FetchApi;
   onClose: () => void;
   onDelete: () => void;
-  onSubmit: (data: Data)  => void;
+  onSubmit: (data: Data) => Promise<void>;
   position?: Position;
 }
 
@@ -34,7 +35,7 @@ export default class PostionForm extends Component<Props, State> {
       }
     };
   }
-  
+
   constructor(props: Props) {
     super(props);
 
@@ -62,26 +63,36 @@ export default class PostionForm extends Component<Props, State> {
         onDelete={this.props.onDelete}
         onSubmit={() => this.props.onSubmit(this.state.position as unknown as Record<string, unknown>)}
         >
-          <>
-            <div>
+          {(errors) => (
+            <>
               <div>
-                Position Code:
-                <input name="code" type="text" value={position.code} />
+                <InputWrapper attribute="code" errors={errors}>
+                  <>
+                    Position Code:
+                    <input name="code" type="text" value={position.code} />
+                  </>
+                </InputWrapper>
+                <InputWrapper attribute="name" errors={errors}>
+                  <>
+                    Position Name:
+                    <input name="name" type="text" value={position.name} />
+                  </>
+                </InputWrapper>
+                <InputWrapper attribute="minimum_pay" errors={errors}>
+                  <>
+                    Minimum Rate:
+                    <input name="minimum_pay" type="number" min="0" step="0.01" value={position.minimum_pay} />
+                  </>
+                </InputWrapper>
+                <InputWrapper attribute="maximum_pay" errors={errors}>
+                  <>
+                    Maximum Rate:
+                    <input name="maximum_pay" type="number" min="0" step="0.01" value={position.maximum_pay} />
+                  </>
+                </InputWrapper>
               </div>
-              <div>
-                Position Name:
-                <input name="name" type="text" value={position.name} />
-              </div>
-              <div>
-                Minimum Rate:
-                <input name="minimum_pay" type="number" min="0" step="0.01" value={position.minimum_pay} />
-              </div>
-              <div>
-                Maximum Rate:
-                <input name="maximum_pay" type="number" min="0" step="0.01" value={position.maximum_pay} />
-              </div>
-            </div>
-          </>
+            </>
+          )}
         </MasterForm>
     );
   }
